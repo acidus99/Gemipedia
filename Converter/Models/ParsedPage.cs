8e543dff1a08386fd28210f6e7a58abd41a18013
Billy Hoffman <acidus99@users.noreply.github.com>
2022-03-27 13:21:54 -0400

    Pages can get all their images

--- Converter/Models/ParsedPage.cs
+++ Converter/Models/ParsedPage.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 
 namespace Gemipedia.Converter.Models
@@ -8,5 +9,24 @@ namespace Gemipedia.Converter.Models
         public String Title { get; set; }
 
         public List<Section> Sections { get; set; } = new List<Section>();
+
+        public List<MediaItem> GetAllImages()
+        {
+            var ret = new List<MediaItem>();
+            foreach(var section in Sections)
+            {
+                CollectorHelper(section, ret);
+            }
+            return ret;
+        }
+
+        private void CollectorHelper(Section section, List<MediaItem> images)
+        {
+            images.AddRange(section.GetItems().Where(x => x is MediaItem).Select(x => (MediaItem)x));
+            foreach(var subSection in section.SubSections)
+            {
+                CollectorHelper(subSection, images);
+            }
+        }
     }
 }
