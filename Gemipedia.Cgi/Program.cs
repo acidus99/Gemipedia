f3b0b303e15dc7b14290a8292b309e4544a76874
Acidus <acidus99@users.noreply.github.com>
2022-05-23 17:08:33 -0400

    Move API/Converter/Renderer into Gemipedia folder

---
+++ Gemipedia.Cgi/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Gemipedia.Cgi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
