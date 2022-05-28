84c86a83978fca1148195e74b952ce10ec23659e
Acidus <acidus99@users.noreply.github.com>
2022-05-28 17:26:37 -0400

    Show type, globe, and link to GeoHack

--- Gemipedia/Renderer/GeoRenderer.cs
+++ Gemipedia/Renderer/GeoRenderer.cs
@@ -22,13 +22,30 @@ namespace Gemipedia.Renderer
             Writer.WriteLine($"=> {CommonUtils.ArticleUrl(geohack.ArticleName)} Back to article");
             Writer.WriteLine();
             Writer.WriteLine($"Place: {geohack.GetPrettyName()}");
-            Writer.WriteLine($"Latitude: {geohack.Latitude.ToString("#.####")}");
-            Writer.WriteLine($"Longitude: {geohack.Longitude.ToString("#.####")}");
+            if(!geohack.IsEarth)
+            {
+                Writer.WriteLine($"Globe: {geohack.Globe}");
+            }
+            if(geohack.HasTypeDescription)
+            {
+                Writer.WriteLine($"Type: {geohack.GetTypeDescription()}");
+            }
+            Writer.WriteLine($"Coordinates:");
+            Writer.WriteLine($"* Latitude: {geohack.Latitude.ToString("#.####")}");
+            Writer.WriteLine($"* Longitude: {geohack.Longitude.ToString("#.####")}");
             Writer.WriteLine();
-            Writer.WriteLine($"=> {OpenStreetMAps(geohack)} Open in OpenStreetMaps.org");
-            Writer.WriteLine($"=> {AppleMapsUrl(geohack)} Open in Apple Maps app");
-            Writer.WriteLine($"=> {GeoUrl(geohack)} Open in default Andriod Maps app (uses geo: URI)");
-            Writer.WriteLine();
+
+            if (geohack.IsEarth)
+            {
+                Writer.WriteLine("## Mapping");
+                Writer.WriteLine($"=> {OpenStreetMAps(geohack)} Open in OpenStreetMaps.org");
+                Writer.WriteLine($"=> {AppleMapsUrl(geohack)} Open in Apple Maps app");
+                Writer.WriteLine($"=> {GeoUrl(geohack)} Open in default Andriod Maps app (uses geo: URI)");
+                Writer.WriteLine();
+            }
+
+            Writer.WriteLine("## Extras");
+            Writer.WriteLine($"=> {geohack.GeohackUrl} Open in GeoHack Launcher");
         }
 
         private string AppleMapsUrl(GeohackParser geohack)
