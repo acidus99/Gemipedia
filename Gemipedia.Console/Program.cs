0b07f5040b609e8e178cdbad23980e5be94ae16a
Acidus <acidus99@users.noreply.github.com>
2022-08-16 18:44:40 -0400

    using Random article to stress test parser

--- Gemipedia.Console/Program.cs
+++ Gemipedia.Console/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+
 using Gemipedia.API;
 using Gemipedia.API.Models;
 using Gemipedia.Converter;
@@ -12,20 +16,74 @@ namespace Gemipedia.Console
 {
     class Program
     {
+        static ThreadSafeCounter counter = new ThreadSafeCounter();
+
         static void Main(string[] args)
         {
+            StressTest();
 
-            var title = (args.Length > 0) ? args[0] : "Area of a circle";
+            do {
+                System.Console.WriteLine("Article?");
+                string name = System.Console.ReadLine();
+                if(name == "")
+                {
+                    return;
+                }
+                var article = GetArticle(name);
+                var newConverter = new WikiHtmlConverter();
 
-            var article = GetArticle(title);
-            var newConverter = new WikiHtmlConverter();
+                ParsedPage page = newConverter.Convert(article.Title, article.HtmlText);
 
-            ParsedPage page = newConverter.Convert(article.Title, article.HtmlText);
+                var renderer = new ArticleRenderer();
+                renderer.RenderArticle(page, System.Console.Out);
 
-            var renderer = new ArticleRenderer();
-            renderer.RenderArticle(page, System.Console.Out);
 
+            } while (true) ;
         }
+        
+        static void StressTest()
+        {
+            int workers = 10;
+            for(int i =0; i < workers; i++)
+            {
+                var thread = new Thread(new ThreadStart(DoStressWork));
+                thread.Start();
+            }
+
+            while(true)
+            {
+                Thread.Sleep(30000);
+            }
+        }
+
+        static void DoStressWork()
+        {
+            var converter = new WikiHtmlConverter();
+
+            var client = new WikipediaApiClient(UserOptions.WikipediaVersion);
+
+            while (true)
+            {
+                var count = counter.Increment();
+                var title = client.GetRandomArticleTitle();
+
+                try
+                {
+                    var article = GetArticle(title);
+                    System.Console.WriteLine($"{count}\t{title}");
+                    ParsedPage page = converter.Convert(article.Title, article.HtmlText);
+
+                    StringWriter fout = new StringWriter();
+                    var renderer = new ArticleRenderer();
+                    renderer.RenderArticle(page, fout);
+                } catch(Exception ex)
+                {
+                    System.IO.File.AppendAllText("/Users/billy/tmp/ERRORS.txt", $"\"{title}\"\t{ex.Message}\n");
+                }
+                System.Threading.Thread.Sleep(100);
+            }
+        }
+
 
         static Article GetArticle(string title)
         {
---
+++ Gemipedia.Console/ThreadSafeCounter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+using System.Threading;
+namespace Gemipedia.Console
+{
+    /// <summary>
+    /// Simple, thread safe counter
+    /// </summary>
+    public class ThreadSafeCounter
+    {
+
+        private int counter;
+
+        public ThreadSafeCounter()
+            : this(0) { }
+
+        public ThreadSafeCounter(int initialValue)
+        {
+            this.counter = initialValue;
+        }
+
+        public int Increment()
+        {
+            int tmp = Interlocked.Increment(ref counter);
+            return tmp;
+        }
+
+        public int Decrement()
+        {
+            int tmp = Interlocked.Decrement(ref counter);
+            return tmp;
+        }
+
+        public int Count
+        {
+            get
+            {
+                return this.counter;
+            }
+        }
+
+
+    }
+}
+
